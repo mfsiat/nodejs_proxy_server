8cb89b90e5a9176ade5ed9af5150722043a385ac
Nasirul Islam <nasirul.islam@northsouth.edu>
2021-12-01 02:28:55 +0600

    init the openwether data

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Routes
+app.use('/api/v1/', require('./routes'));
+
+// Enable CORS
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
